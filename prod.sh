eb4d94aec96f7a95a663562647022209718751e8
atliSig <atlisigurgeirsson@gmail.com>
2020-02-28 15:45:19 +0000

    updated production settings

--- prod.sh
+++ prod.sh
@@ -4,4 +4,10 @@ export FLASK_ENV=production
 export PORT=7331
 export SEMI_PROD=True
 
-gunicorn -b 127.0.0.1:$PORT wsgi --timeout 1500 --workers 4 --threads 4
+gunicorn -b 127.0.0.1:$PORT wsgi \
+    --timeout 360 \
+    --workers 4 \
+    --threads 4 \
+    --log-level=debug \
+    --error-logfile=gunicorn.log \
+    --capture-output \

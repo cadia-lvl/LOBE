f8f81612f1f231e2e1b44b4270a73f6baac3e17e
Þorsteinn Daði Gunnarsson <thorsteinng@ru.is>
2020-07-04 02:44:52 +0000

    Add defaults before running in prod script

--- prod.sh
+++ prod.sh
@@ -4,10 +4,13 @@ export FLASK_ENV=production
 export PORT=7331
 export SEMI_PROD=True
 
+python manage.py add_default_roles
+python manage.py add_default_configuration
+
 gunicorn -b 127.0.0.1:$PORT wsgi \
     --timeout 360 \
     --workers 4 \
     --threads 4 \
     --log-level=debug \
     --error-logfile=gunicorn.log \
-    --capture-output \
+    --capture-output
